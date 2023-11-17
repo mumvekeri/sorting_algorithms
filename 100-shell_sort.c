b79546801452fd688437b8ed56ba5f16789341a8
mumvekeri <mumvekerichidochashe@gmail.com>
2023-11-17 02:07:11 -0800

    try

--- 100-shell_sort.c
+++ 100-shell_sort.c
@@ -11,19 +11,19 @@ void shell_sort(int *array, size_t size)
 {
 	size_t interval = 1, i, j;
 	int temp;
-	
+
 	if (array == NULL || size < 2)
 		return;
-	
+
 	while (interval < size / 3)
 		interval = interval * 3 + 1;
-	
+
 	while (interval > 0)
 	{
 		for (i = interval; i < size; i++)
 		{
 			temp = array[i];
-			
+
 			for (j = i; j >= interval && array[j - interval] >
 					temp; j -= interval)
 			{
