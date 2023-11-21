96005648c5438ed8a6328e4fc1b54b65d9dc3b0c
mumvekeri <mumvekerichidochashe@gmail.com>
2023-11-21 08:13:51 -0800

    try

--- sort.h
+++ sort.h
@@ -44,4 +44,5 @@ void swaps(int *array, size_t i, size_t j, size_t size);
 void radix_sort(int *array, size_t size);
 void counting_sort_radix(int *array, size_t size, int exp);
 int get_max(int *array, size_t size);
+
 #endif /* _SORT_H_ */
