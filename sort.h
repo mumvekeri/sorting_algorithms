969c7ba07c92c0a3d9173808aabf41b17dec435e
mumvekeri <mumvekerichidochashe@gmail.com>
2023-11-21 08:24:58 -0800

    try

--- sort.h
+++ sort.h
@@ -2,9 +2,7 @@
 #define SORT_H
 
 #include <stdio.h>
-#include <stddef.h>
 #include <stdlib.h>
-#include <stdbool.h>
 
 /**
  * struct listint_s - Doubly linked list node
@@ -19,6 +17,17 @@ typedef struct listint_s
     struct listint_s *prev;
     struct listint_s *next;
 } listint_t;
+
+/**
+ * enum bool - Enumeration of Boolean values.
+ * @false: Equals 0.
+ * @true: Equals 1.
+ */
+typedef enum bool
+{
+	false = 0,
+	true
+} bool;
 
 void bubble_sort(int *array, size_t size);
 void swap(int *a, int *b);
